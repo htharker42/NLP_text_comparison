431efb55520bdf7eaed61acbde6d58faf52051e4
htharker42 <htharker@gmail.com>
2019-02-20 13:30:38 -0800

    added basic api

--- app.py
+++ app.py
@@ -1,20 +1,73 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-import os
+import bcrypt
+import spacy
 from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
-db = client.aNewDB
-UserNum.insert({
-    'num_of_users': 0 
-})
+db = client.SimilarDB
+users = db["Users"]
+
+def UserExist(username):
+    if user.find({"Username": username }).count() == 0:
+        return False
+    else:
+        return True
+
+def verifyPW(username, password):
+    return true
+
+class Register(Resource):
+    def post(self)
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData['password']
+
+        if (UserExist(username)): 
+            retJson = {
+                "status": "301",
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+        
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw
+        })
+
+        retJson = {
+            'status': 200,
+            'msg': 'registration successful'
+        }
+
+
+
+class Detect(Resource):
+    def post(self)
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        text1 = postedData["text1"]
+        text2 = postedData["text2"]
+
+        if not UserExist(username):
+            retJson = {
+                "status": "301",
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+        
 
 @app.route('/')
 def running():
     return "I am fully operational and all my systems are functioning perfectly"
+
 
 api.add_resource(Visit, "/hello")
 
